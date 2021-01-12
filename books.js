89b4f7c07761d38562a23449bd881508ba1b45be
githubofjk <hkim317@gmail.com>
2021-01-12 15:31:54 -0800

    Initial book.js

---
+++ books.js
@@ -0,0 +1,9 @@
+function Book(name, author, pages, read) {
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+        return `${name} by ${author}, ${pages} pages, ${read}`;
+    }
+}
