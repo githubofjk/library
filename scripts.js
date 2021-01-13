844945f1d5bcc36945308b0208128ecda9fdfa9c
githubofjk <hkim317@gmail.com>
2021-01-12 16:09:42 -0800

    Initial scripts.js displayMyLibraryBooks

--- scripts.js
+++ scripts.js
@@ -10,10 +10,25 @@ function Book(name, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
-    let newBook = new Book("Hobbit", "JR", "234", "false");
+function addBookToLibrary(name, author, pages, read) {
+    let newBook = new Book(name, author, pages, read);
     myLibrary.push(newBook);
-    console.log(myLibrary);
+    let newBook2 = new Book("LOTR", "JR", "567", "false");
+    myLibrary.push(newBook2);
 }
 
-window.addEventListener("DOMContentLoaded", addBookToLibrary);
+function displayMyLibraryBooks() {
+    const library = document.querySelector("#library");
+    
+    myLibrary.forEach((book) => {
+        const divBook = document.createElement('div');
+        console.log(book);
+        divBook.textContent = `${book["name"]} ${book["author"]} ${book["pages"]} ${book["read"]}`;
+        library.appendChild(divBook);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    addBookToLibrary("Hobbit", "JR", "234", "false");
+    displayMyLibraryBooks();
+});
